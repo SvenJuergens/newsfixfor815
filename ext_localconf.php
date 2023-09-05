17e67095ea2fed5b70089a981bca7d1d85aa3a9a
SvenJuergens <SvenJuergens@users.noreply.github.com>
2023-09-06 00:35:48 +0200

    [TASK] rector changes

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['Domain/Repository/AbstractDemandedRepository.php']['findDemanded'][] =
     \SvenJuergens\Newsfixfor815\Hooks\FindDemanded::class . '->find';
